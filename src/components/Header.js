67a22f22accfc3ed69b8265fb77ff0b8eddf4f70
robsonsilv4 <robsonsilv410@gmail.com>
2018-10-12 14:38:04 -0300

    Estilizando o Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 /*
 Statefull:
@@ -13,11 +13,39 @@ class Component exteds React.Component {
 }
 */
 
+/*
+const containerStyle = {
+	marginTop: 15,
+	backgroundColor: '#fff',
+	alignItens: 'center',
+	justifyContent: 'center'
+}
+
+const titleStyle = {
+	color: '#fff',
+	fontSize: 50
+}
+*/
+
 // Stateless:
 const Header = (props) => (
-	<View>
-		<Text>Esse é o nosso Header</Text>
+	<View style={style.container}>
+		<Text style={style.title}>Esse é o nosso Header</Text>
 	</View>
 );
+
+const style = StyleSheet.create({
+	container: {
+		marginTop: 15,
+		backgroundColor: '#fff',
+		/*Flex Box*/
+		alignItens: 'center',
+		justifyContent: 'center'
+	},
+	title: {
+		color: '#fff',
+		fontSize: 50
+	}
+});
 
 export default Header;
